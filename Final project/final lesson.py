f6460ad61a37d837f3ace6d905a2b46a03c4bf3e
tigr737 <tigr737@yandex.ru>
2024-11-12 22:40:45 +0500

    add requirements.txt

--- Final project/final lesson.py
+++ Final project/final lesson.py
@@ -1,15 +1,7 @@
-"""Игра угадай число
-Компьютер сам загадывает и сам угадывает число
-"""
-
 import numpy as np
 
-
 def game_core_v3(number: int = 1) -> int:
-    """Сначала устанавливаем любое random число, а потом уменьшаем
-    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток
-
+    """
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -17,17 +9,16 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-    predict = np.random.randint(1, 101)
+    predict = np.random.randint(1, 101) # предполагаемое число
 
     while number != predict:
         count += 1
         if number < predict:
             predict = predict//2
-        
-        if number > predict:
-            predict = predict*2
         else:
-            predict -= 1        
+            predict += 1
+        if number > predict:
+            predict += 1    
     return count
 
 
@@ -51,5 +42,5 @@ def score_game(game_core_v3) -> int:
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
     
-    #  RUN
+print('Run benchmarking for game_core_v3: ', end='')
 score_game(game_core_v3)
